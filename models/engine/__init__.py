39f1ec382801a2a2836926bd1ea3367edaf6538d
Lawrence Mugwena <mugwenalawrence121@gmail.com>
2022-11-26 18:09:23 +0200

    file_storage file update, new fuctionality added

--- models/engine/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" modules imported """
-
-from models.engine.file_storage import FileStorage
-
-#filestorage instatance
-storage = FileStorage()
-storage.save(self)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,29 +1,55 @@
 #!/urs/bin/python3
 """ modules imported """
 
+from json import dump, load, dumps
+from os.path import exists
+from models.base_model import BaseModel
+from models.city import City
+from  models.state import State 
+from models.review import Review
+from models.amenity import Amenity
+from models.place import Place
+from models.user import User
 
-class FileStorange:
+list_of_classes =[BaseModel, User, State, City, Place, Amenity, Review]
+
+class FileStorage:
     """File Storage class definition"""
 
-    __file_path = "./file.json"
+    __file_path = "file.json"
     __objects = {}
 
     def all(self):
         """returns a dictinary"""
-        return self.__objects
+        return FileStorage.__objects
 
-    def new(self):
+    def new(self, obj):
         """
         sets in  __objects the object with key
          <class name>.id
         """
-        pass
+        class_name = obj.__class__.__name__
+        id = obj.id
+        class_id = class_name + "." + id
+        FileStorage.__objects[class_id] = obj
 
     def save(self):
         """serializes __obejects  to the json file"""
-        pass
+        dict_to_json = {}
+        for key, value in FileStorage.__objects.items():
+            dict_to_json[key] = value.to_dict()
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
+            dump(dict_to_json, file)
 
     def reload(self):
         """deserializes the JSON file to __objects"""
-        pass
-    
+        dict_obj = {}
+        FileStorage.__objects = {}
+        if (exists(FileStorage.__file_path)):
+            for key, value in dict_obj.items():
+                class_name = key.split(".")[0]
+                if class_name in list_of_classes:
+                    FileStorage.__objects[key] = eval(class_name)(**value)
+                else:
+                    pass
+                
