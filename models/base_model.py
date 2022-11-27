7120134a6e1ab0d09e2d80df226491de077565e3
Joseph Alikah <alikahjoseph@gmail.com>
2022-11-27 23:41:14 +0100

    Fix Base Model

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" datetime, uuid modules """
+"""Base Model."""
 
 from datetime import datetime
 import uuid
@@ -9,9 +9,10 @@ DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel:
-    """BaseModel definition"""
+    """BaseModel definition."""
 
     def __init__(self, *args, **kwargs):
+        """Initialize the base model."""
         if kwargs:
             for key, value in kwargs.items():
                 if key in ["created_at", "updated_at"]:
@@ -20,20 +21,20 @@ class BaseModel:
                 if key not in ["__class__"]:
                     setattr(self, key, value)
         else:
-            self.id = uuid.uuid4()
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
-        """string formatter"""
+        """Return the string representation."""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """ " udpate updated_at attribute"""
+        """Set the updated_at attribute."""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """" to_dic method definition"""
+        """Return the the dictionary representation."""
         dic = {}
 
         for key, item in self.__dict__.items():
