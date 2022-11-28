6c4c3169292c7e36e21ae1f8543366ebdd2b215c
Lawrence Mugwena <mugwenalawrence121@gmail.com>
2022-11-29 01:47:06 +0200

    __init__ update

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-""" FileStorage instance """
+"""Initializes the package"""
 
 from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.review import Review
-from models.amenity import Amenity
-from models.review import Review
 
 storage = FileStorage()
 storage.reload()
