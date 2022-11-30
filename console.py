6c335f8387ec00691d0cace33f18648d6b78df0c
Lawrence Mugwena <mugwenalawrence121@gmail.com>
2022-11-30 03:43:57 +0200

    console update

--- console.py
+++ console.py
@@ -1,2 +1,123 @@
 #!/usr/bin/python3
+""" imported modules """
+import cmd, shlex
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.city import City
 
+classes = {
+    'Basemodel': BaseModel,
+    'User': User,
+    'State': State,
+    'Place': Place,
+    'Review': Review,
+    'City': City,
+    'Amenity': Amenity,
+}
+
+class HBNBCommand(cmd.Cmd):
+    """ Defines the Interpreter class HBNBCommand """
+    prompt = "hbnb "
+    
+    def do_quit(self):
+        """ Quit command and exit the program """
+        return True
+
+    def do_EOF(self, line):
+        """ Exit the program in non-interactive mode """
+        return True
+
+    def do_create(self, args):
+        """ create instance of a model """
+        args = shlex.split(args)
+        if not (args):
+            print("** class name missing **")
+        elif args[0] not in classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            temp = eval(args[0])()
+            temp.save()
+        else:
+            print("** Too many arguments for create **")
+    
+    def do_show(self, args):
+        """ Prints the string representation of and instance """
+        if not args:
+            args = shlex.split(args)
+            if len(args) != 2:
+                print("** instance id missing **")
+            elif args[0] not in classes:
+                print("** class doesn't exits **")
+            else:
+                for key, value in storage.all().items():
+                    if args[1] == value.id:
+                        print(value)
+                        return
+                    print("** no instance found **")
+    
+    def do_destroy(self, args):
+        """ Deletes the instance that matched the class id and name """
+        args = shlex.split(args)
+        if not args:
+            print("** class name missing **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        elif args[0] not in classes:
+            print("** class doesn't exist **")
+        else:
+            for key, value in storage.all().items():
+                if args[1] == value.id:
+                    del storage.all()[key]
+                    storage.save()
+            print("** no instance found **")
+    
+    def do_all(self, args):
+        """ Prints all string representation of all instances """
+        args = shlex.split(args)
+        n_list = []
+        if args:
+            try:
+                for key in storage.all():
+                    if args[0] == key.split('.')[0]:
+                        n_list.append(str(storage.all()[key]))
+                print(n_list)
+            except ModuleNotFoundError:
+                print("** class doesn't exist **")
+        else:
+            for key in storage.all():
+                n_list.append(str(storage.all()))
+            print(n_list)
+    
+    def do_update(self, args):
+        """ Update an instance based of the class name and id """
+        args = args.split()
+        if len(args) == 0:
+            print("** class name missing **")
+            return False
+        if args[0] in classes:
+            if len(args) > 1:
+                key = args[0] + '.' + args[1]
+                if key in storage.all():
+                    if len(args) > 2:
+                        if len(args) > 3:
+                            setattr(storage.all()[key], args[2], args[3])
+                            storage.all()[key].save()
+                        else:
+                            print("** value missing **")
+                    else:
+                        print("** attribute name missing **")
+                else:
+                    print("** no instance found **")
+            else:
+                print("** instance doesn't exist **")
+        else:
+            print("** class doesn't exist **")
+        
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
+ 
