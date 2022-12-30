4aa35fb5a94a06bcecf96d41abb48be637485acb
Joseph Alikah <alikahjoseph@gmail.com>
2022-12-30 13:29:24 +0100

    Fix Place model

--- models/place.py
+++ models/place.py
@@ -11,8 +11,8 @@ class Place(BaseModel):
     user_id = ""
     name = ""
     description = ""
-    number_room = ""
-    number_bathroom = ""
+    number_room = 0
+    number_bathroom = 0
     max_guest = 0
     price_by_night = 0
     latitude = 0.0
